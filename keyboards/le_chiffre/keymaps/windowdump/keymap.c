ecf9660cccced1086346c2d8888a917a4a98cc26
Window Dump <18600035+WindowDump@users.noreply.github.com>
2021-06-20 14:04:35 -0400

    more combo tweaks

--- keyboards/le_chiffre/keymaps/windowdump/keymap.c
+++ keyboards/le_chiffre/keymaps/windowdump/keymap.c
@@ -1,64 +1,112 @@
-/* Copyright 2020 tominabox1
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
+#include "windowdump.h"
 
-enum layers{
-  _BASE,
-  _NUM_SYM,
-  _NAV
-};
-
-enum combo_events {
-  COMBO_BSPC,
-  COMBO_NUMBAK,
-  COMBO_TAB,
-  COMBO_ESC,
-  COMBO_DEL,
-};
-
-#define KC_NUM_SPC LT(_NUM_SYM, KC_SPC)
-#define KC_GA LGUI_T(KC_A)
-#define KC_AS LALT_T(KC_S)
-#define KC_CD LCTL_T(KC_D)
-#define KC_SF LSFT_T(KC_F)
-#define KC_SJ RSFT_T(KC_J)
-#define KC_CK RCTL_T(KC_K)
-#define KC_AL RALT_T(KC_L)
-#define KC_GSCLN RGUI_T(KC_SCLN)
+#define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT(
-    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,  KC_MPLY,  KC_Y,   KC_U,    KC_I,   KC_O,     KC_P,
-   KC_GA,  KC_AS,  KC_CD,  KC_SF,   KC_G,            KC_H,  KC_SJ,   KC_CK,  KC_AL, KC_GSCLN,
-    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,            KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH,
-                         KC_LCTL, KC_ENT,            KC_NUM_SPC, MO(_NAV)
+  [_QWERTY] = LAYOUT_wrapper(
+    _________________QWERTY_L1_________________,  KC_MPLY,  _________________QWERTY_R1_________________,
+    _________________QWERTY_L2_________________,            _________________QWERTY_R2_________________,
+    _________________QWERTY_L3_________________,            _________________QWERTY_R3_________________,
+                               KC_LOWR, KC_NMBS,            KC_NVSP, KC_RASE
   ),
 
-  [_NUM_SYM] = LAYOUT(
-        KC_1,  KC_2,     KC_3,     KC_4,      KC_5,  KC_TRNS,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-    KC_EXLM,  KC_AT,  KC_HASH,   KC_DLR,   KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR, KC_EQUAL,  KC_MINS,
-    KC_BSLS,KC_LCBR,  KC_LBRC,  KC_LPRN,   KC_UNDS,            KC_RPRN,  KC_RBRC,  KC_RCBR,   KC_DOT,   KC_GRV,
-                                KC_CAPS,   KC_TRNS,            KC_TRNS,  KC_TRNS
+  [_QWERTY_MODS] = LAYOUT_wrapper(
+    ___________________BLANK___________________,  _______,  ___________________BLANK___________________,
+    ____________QWERTY_L2_NO_HR_MODS___________,            ____________QWERTY_R2_NO_HR_MODS___________,
+    ___________________BLANK___________________,            ___________________BLANK___________________,
+                               _______, _______,            _______, _______
   ),
 
-  [_NAV] = LAYOUT(
-      RESET,  _______,  AG_NORM,  AG_SWAP,  DEBUG, KC_TRNS,   KC_GRV,  KC_PGDN,    KC_UP,  KC_PGUP,  KC_SCLN,
-    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  KC_NO,           KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,
-    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_NO,           KC_MINS,    KC_RO,  KC_COMM,   KC_DOT,  KC_BSLS,
-                                  KC_TRNS,KC_TRNS,           KC_TRNS,  KC_TRNS
+  [_COLEMAK] = LAYOUT_wrapper(
+    ______________COLEMAK_MOD_DH_L1____________,  _______,  ______________COLEMAK_MOD_DH_R1____________,
+    ______________COLEMAK_MOD_DH_L2____________,            ______________COLEMAK_MOD_DH_R2____________,
+    ______________COLEMAK_MOD_DH_L3____________,            ______________COLEMAK_MOD_DH_R3____________,
+                               _______, _______,            _______, _______
+  ),
+
+  [_COLEMAK_MODS] = LAYOUT_wrapper(
+    ___________________BLANK___________________,  _______,  ___________________BLANK___________________,
+    ________COLEMAK_MOD_DH_L2_NO_HR_MODS_______,            ________COLEMAK_MOD_DH_R2_NO_HR_MODS_______,
+    ___________________BLANK___________________,            ___________________BLANK___________________,
+                               _______, _______,            _______, _______
+  ),
+
+  [_HANDSDOWN] = LAYOUT_wrapper(
+    ______________HANDS_DOWN_ALT_L1____________,  _______,  ______________HANDS_DOWN_ALT_R1____________,
+    ______________HANDS_DOWN_ALT_L2____________,            ______________HANDS_DOWN_ALT_R2____________,
+    ______________HANDS_DOWN_ALT_L3____________,            ______________HANDS_DOWN_ALT_R3____________,
+                               _______, _______,            _______, _______
+  ),
+
+  [_HANDSDOWN_MODS] = LAYOUT_wrapper(
+    ___________________BLANK___________________,  _______,  ___________________BLANK___________________,
+    ________HANDS_DOWN_ALT_L2_NO_HR_MODS_______,            ________HANDS_DOWN_ALT_R2_NO_HR_MODS_______,
+    ___________________BLANK___________________,            ___________________BLANK___________________,
+                               _______, _______,            _______, _______
+  ),
+
+  [_GAMER] = LAYOUT_wrapper(
+    ___________________BLANK___________________,  KC_ESC,   ___________________BLANK___________________,
+    ____________QWERTY_L2_NO_HR_MODS___________,            ___________________BLANK___________________,
+    ___________________BLANK___________________,            ___________________BLANK___________________,
+                               KC_GLWR, KC_BSPC,            _______, _______
+  ),
+
+  [_GAMER2] = LAYOUT_wrapper(
+    ___________________BLANK___________________,  KC_ESC,   ___________________BLANK___________________,
+    ___________________BLANK___________________,            ___________________BLANK___________________,
+    ___________________BLANK___________________,            ___________________BLANK___________________,
+                               KC_GLWR, KC_SPC,             KC_NVBS, _______
+  ),
+
+  [_TOUHOU] = LAYOUT_wrapper(
+    KC_ESC,  _______, _______, _______, _______,  _______,  _______, _______, KC_UP,   _______, _______,
+    ________________TOUHOU_LEFT________________,            ________________TOUHOU_RIGHT_______________,
+    ___________________BLANK___________________,            ___________________BLANK___________________,
+                               _______, _______,            _______, _______
+  ),
+
+  [_NUMBERS] = LAYOUT_wrapper(
+    _______________SYMBOLS_LEFT________________,  _______,  _______________SYMBOLS_RIGHT_______________,
+    ________________NUMBER_LEFT________________,            ________________NUMBER_RIGHT_______________,
+    _________________FUNC_LEFT_________________,            _________________FUNC_RIGHT________________,
+                               _______, V_V_V_V,            KC_F11,  KC_F12
+  ),
+
+  [_NAV] = LAYOUT_wrapper(
+    __________________NAV_L1___________________,  _______,  __________________NAV_R1___________________,
+    __________________NAV_L2___________________,            __________________NAV_R2___________________,
+    __________________NAV_L3___________________,            __________________NAV_R3___________________,
+                               _______, _______,            V_V_V_V, _______
+  ),
+
+  [_GLOWER] = LAYOUT_wrapper(
+    ________________NUMBER_RIGHT_______________,  _______,  _________________LOWER_R1__________________,
+    ________________NUMBER_LEFT________________,            _________________LOWER_R2__________________,
+    _________________FUNC_LEFT_________________,            _________________FUNC_RIGHT________________,
+                               V_V_V_V, _______,            KC_F11, KC_ADJS
+  ),
+
+  [_LOWER] = LAYOUT_wrapper(
+    _________________LOWER_L1__________________,  _______,  _________________LOWER_R1__________________,
+    _________________LOWER_L2__________________,            _________________LOWER_R2__________________,
+    _________________LOWER_L3__________________,            _________________LOWER_R3__________________,
+                               V_V_V_V, _______,            KC_DEL,  KC_ADJS
+  ),
+
+  [_RAISE] = LAYOUT_wrapper(
+    _________________RAISE_L1__________________,  KC_PSCR,  _________________RAISE_R1__________________,
+    _________________RAISE_L2__________________,            _________________RAISE_R2__________________,
+    _________________RAISE_L3__________________,            _________________RAISE_R3__________________,
+                               KC_ADJS, KC_DEL,             _______, V_V_V_V
+  ),
+
+  [_ADJUST] = LAYOUT_wrapper(
+    _________________ADJUST_L1_________________,  _______,  _________________ADJUST_R1_________________,
+    _________________ADJUST_L2_________________,            _________________ADJUST_R2_________________,
+    _________________ADJUST_L3_________________,            _________________ADJUST_R3_________________,
+                               V_V_V_V, _______,            _______, V_V_V_V
   ),
 };
 
@@ -72,131 +120,12 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     }
 }
 
-#ifdef COMBO_ENABLE
-const uint16_t PROGMEM combo_bspc[] = {KC_O, KC_P, COMBO_END};
-const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
-const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_esc[] = {KC_E, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
-
-combo_t key_combos[COMBO_COUNT] = {
-  [COMBO_BSPC] = COMBO(combo_bspc,KC_BSPC),
-  [COMBO_NUMBAK] = COMBO(combo_numbak,KC_BSPC),
-  [COMBO_TAB] = COMBO(combo_tab,KC_TAB),
-  [COMBO_ESC] = COMBO(combo_esc,KC_ESC),
-  [COMBO_DEL] = COMBO(combo_del,KC_DEL),
-
-};
-#endif
-
-#ifdef OLED_DRIVER_ENABLE  //Special thanks to Sickbabies for this great OLED widget!
+#ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_90;  // rotates for proper orientation
 }
 
-void render_lechiffre_logo(void) {
-    static const char PROGMEM lechiffre_logo[] = {
-	// 'lechiffre_logo', 32x20px
-   0x00, 0x3e, 0x20, 0x20, 0x00, 0x18, 0x2c, 0xa8, 0x80, 0x00, 0x1c, 0x22, 0x22, 0x00, 0x3e, 0x08,
-0x30, 0x00, 0x34, 0x00, 0x3c, 0x0a, 0x00, 0xbc, 0x8a, 0x00, 0x38, 0x08, 0x00, 0x18, 0x2c, 0x28,
-0x00, 0xb6, 0xb6, 0x00, 0xdb, 0xdb, 0x00, 0x6d, 0x6d, 0x00, 0xdb, 0xdb, 0x00, 0xdb, 0xdb, 0x00,
-0x00, 0xdb, 0xdb, 0x00, 0xdb, 0xdb, 0x00, 0x6d, 0x6d, 0x00, 0xdb, 0xdb, 0x00, 0xb6, 0xb6, 0x00,
-0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x06, 0x06, 0x06, 0x00, 0x06, 0x06, 0x06, 0x06, 0x06, 0x00,
-0x00, 0x06, 0x06, 0x06, 0x06, 0x06, 0x00, 0x06, 0x06, 0x06, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00
-};
-
-  oled_write_raw_P(lechiffre_logo, sizeof(lechiffre_logo));
-}
-
-static void render_layer_status(void) {
-    oled_write_P(PSTR("-----"), false);
-    switch (get_highest_layer(layer_state)) {
-        case _BASE:
-            oled_write_ln_P(PSTR("BASE"), false);
-            break;
-        case _NUM_SYM:
-            oled_write_ln_P(PSTR(" SYM"), false);
-            break;
-        case _NAV:
-            oled_write_ln_P(PSTR(" NAV"), false);
-            break;
-        default:
-            oled_write_ln_P(PSTR("?????"), false);
-    }
-}
-
-#    define KEYLOG_LEN 11
-char     keylog_str[KEYLOG_LEN] = {};
-uint8_t  keylogs_str_idx        = 0;
-uint16_t log_timer              = 0;
-
-const char code_to_name[60] = {
-    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
-
-void add_keylog(uint16_t keycode) {
-    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) || (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
-        keycode = keycode & 0xFF;
-    }
-
-    for (uint8_t i = KEYLOG_LEN - 1; i > 0; i--) {
-        keylog_str[i] = keylog_str[i - 1];
-    }
-    if (keycode < 60) {
-        keylog_str[0] = code_to_name[keycode];
-    }
-    keylog_str[KEYLOG_LEN - 1] = 0;
-
-    log_timer = timer_read();
-}
-
-void update_log(void) {
-    if (timer_elapsed(log_timer) > 750) {
-        add_keylog(0);
-    }
-}
-
-//Text only renders
-void render_keylogger_status(void) {
-    oled_write_P(PSTR("-----"), false);
-    oled_write(keylog_str, false);
-}
-
-void render_keylock_status(led_t led_state) {
-    oled_write_P(PSTR("-----"), false);
-    oled_write_P(PSTR("C"), led_state.caps_lock);
-	oled_write_P(PSTR(" "), false);
-    oled_write_P(PSTR("N"), led_state.num_lock);
-	oled_write_P(PSTR(" "), false);
-    oled_write_P(PSTR("S"), led_state.scroll_lock);
-    //oled_write_ln_P(PSTR(" "), false);
-}
-
-void render_mod_status(uint8_t modifiers) {
-       oled_write_P(PSTR("-----"), false);
-    oled_write_ln_P(PSTR("SHFT"), (modifiers & MOD_MASK_SHIFT));
-    oled_write_ln_P(PSTR("ALT"), (modifiers & MOD_MASK_ALT));
-    oled_write_ln_P(PSTR("CTRL"), (modifiers & MOD_MASK_CTRL));
-    oled_write_ln_P(PSTR("GUI"), (modifiers & MOD_MASK_GUI));
-}
-
 void oled_task_user(void) {
-    render_lechiffre_logo();
-    oled_set_cursor(0,3);
-    render_layer_status();	// Renders the current keyboard state (layer, lock, caps, scroll, etc)
-	render_mod_status(get_mods()|get_oneshot_mods());
-	render_keylock_status(host_keyboard_led_state());
-	render_keylogger_status();
+    oled_render_layer_state();
 }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-      add_keylog(keycode);
-    }
-    return true;
-  }
 #endif
